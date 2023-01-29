03e422cabba3ee8e82e0e6f20cd946660d341ef5
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-01-30 00:29:36 +0600

    seconr nav added

--- src/components/share/navbar/Navbar.js
+++ src/components/share/navbar/Navbar.js
@@ -5,6 +5,7 @@ import { BiSearch } from 'react-icons/bi';
 import { TiUserOutline } from 'react-icons/ti';
 import { BsBasket } from 'react-icons/bs';
 import logo from "../../../images/footerimg/logo.png";
+import SecondNav from './SecondNav';
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -14,31 +15,34 @@ const Navbar = () => {
     };
 
     return (
-        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 '>
-            <div>
-                <img src={logo} alt="" />
-            </div>
-            <div className='hidden lg:block'>
-                <div className='flex items-center '>
-                    <input className='border border-[#F1F1F1] bg-[#F6F6F6] py-1 px-2' type="text" placeholder="I'm Lookin for..." />
-                    <button className='bg-[#0198E9] py-2 px-2'><BiSearch className='text-white' /></button>
+        <>
+            <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 '>
+                <div>
+                    <img src={logo} alt="" />
+                </div>
+                <div className='hidden lg:block'>
+                    <div className='flex items-center '>
+                        <input className='border border-[#F1F1F1] bg-[#F6F6F6] py-1 px-2' type="text" placeholder="I'm Lookin for..." />
+                        <button className='bg-[#0198E9] py-2 px-2'><BiSearch className='text-white' /></button>
+                    </div>
+                </div>
+                <div className='hidden lg:flex gap-4'>
+                    <NavLink className="flex items-center gap-1"><TiUserOutline /><small>Login</small></NavLink>
+                    <NavLink className="flex items-center gap-1"><AiOutlineHeart /> <small>Wishlist</small></NavLink>
+                    <NavLink className="flex items-center gap-1"><BsBasket /><small>My Cart</small> <div className='bg-[#FF5D5D] w-[14px] h-[14px] rounded-full relative'><small className='absolute mt-[-1px] ml-[5px] text-xs text-white'>1</small></div></NavLink>
+                </div>
+
+                <div onClick={handleNav} className='block lg:hidden'>
+                    {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+                </div>
+                <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+                    <NavLink className="flex items-center gap-1 text-white p-4 border-b border-gray-600"><TiUserOutline /><small>Login</small></NavLink>
+                    <NavLink className="flex items-center gap-1 text-white p-4 border-b border-gray-600"><AiOutlineHeart /> <small>Wishlist</small></NavLink>
+                    <NavLink className="flex items-center gap-1 text-white p-4 border-b border-gray-600"><BsBasket /><small>My Cart</small> <div className='bg-[#FF5D5D] w-[14px] h-[14px] rounded-full relative'><small className='absolute mt-[-1px] ml-[5px] text-xs text-white'>1</small></div></NavLink>
                 </div>
             </div>
-            <div className='hidden lg:flex gap-4'>
-                <NavLink className="flex items-center gap-1"><TiUserOutline /><small>Login</small></NavLink>
-                <NavLink className="flex items-center gap-1"><AiOutlineHeart /> <small>Wishlist</small></NavLink>
-                <NavLink className="flex items-center gap-1"><BsBasket /><small>My Cart</small> <div className='bg-[#FF5D5D] w-[14px] h-[14px] rounded-full relative'><small className='absolute mt-[-1px] ml-[5px] text-xs text-white'>1</small></div></NavLink>
-            </div>
-
-            <div onClick={handleNav} className='block lg:hidden'>
-                {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-            </div>
-            <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-                <NavLink className="flex items-center gap-1 text-white p-4 border-b border-gray-600"><TiUserOutline /><small>Login</small></NavLink>
-                <NavLink className="flex items-center gap-1 text-white p-4 border-b border-gray-600"><AiOutlineHeart /> <small>Wishlist</small></NavLink>
-                <NavLink className="flex items-center gap-1 text-white p-4 border-b border-gray-600"><BsBasket /><small>My Cart</small> <div className='bg-[#FF5D5D] w-[14px] h-[14px] rounded-full relative'><small className='absolute mt-[-1px] ml-[5px] text-xs text-white'>1</small></div></NavLink>
-            </ul>
-        </div>
+            <SecondNav />
+        </>
     );
 };
 
---
+++ src/components/share/navbar/SecondNav.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { NavLink } from 'react-router-dom';
+
+const SecondNav = () => {
+    const [nav, setNav] = useState(false);
+
+    const handleNav = () => {
+        setNav(!nav);
+    };
+
+    return (
+        <>
+            <div className='flex items-center  max-w-[1240px] mx-auto px-4'>
+                <div className='hidden md:flex gap-10 ml-12'>
+                    <NavLink className="font-medium text-sm">Women's Fashion</NavLink>
+                    <NavLink className="font-medium text-sm">men's Fashion</NavLink>
+                    <NavLink className="font-medium text-sm">Kid's Fashion</NavLink>
+                    <NavLink className="font-medium text-sm">Home & Lifestyle</NavLink>
+                    <NavLink className="font-medium text-sm">Arts & Crafts</NavLink>
+                    <NavLink className="font-medium text-sm">Computer & Electronics</NavLink>
+                    <NavLink className="font-medium text-sm">Food & Grocery</NavLink>
+                </div>
+                <div onClick={handleNav} className='block md:hidden'>
+                    {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+                </div>
+                <ul className={nav ? 'fixed right-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed right-[100%]'}>
+                    <li className='p-4 border-b border-gray-600'><NavLink className="text-white">Women's Fashion</NavLink></li>
+                    <li className='p-4 border-b border-gray-600 '><NavLink className="text-white">men's Fashion</NavLink></li>
+                    <li className='p-4 border-b border-gray-600 '><NavLink className="text-white">Kid's Fashion</NavLink></li>
+                    <li className='p-4 border-b border-gray-600 '><NavLink className="text-white">Home & Lifestyle</NavLink></li>
+                    <li className='p-4 border-b border-gray-600 '><NavLink className="text-white">Arts & Crafts</NavLink></li>
+                    <li className='p-4 border-b border-gray-600 '><NavLink className="text-white">Computer & Electronics</NavLink></li>
+                    <li className='p-4 border-b border-gray-600 '><NavLink className="text-white">Food & Grocery</NavLink></li>
+                </ul>
+            </div>
+            <hr className='my-2' />
+        </>
+    );
+};
+
+export default SecondNav;
