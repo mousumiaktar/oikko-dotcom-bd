93717c5733cac1289eab161c20683f94ec73919d
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-01-29 12:45:38 +0600

    cart page

---
+++ src/components/pages/cart/Cart.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Container from '../Container';
+
+const Cart = () => {
+    return (
+        <Container>
+            <section className='md:flex lg:flex'>
+                <div className=' w-full md:w-[82%] mx-auto'>
+
+                </div>
+                <div className='w-[300px] mr-3'>
+
+                </div>
+            </section>
+        </Container>
+    );
+};
+
+export default Cart;
