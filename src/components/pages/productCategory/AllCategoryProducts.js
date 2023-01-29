9b2472a1ae53c6211a97296961f65bf705b5cda2
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-01-29 12:33:50 +0600

    all category added

---
+++ src/components/pages/productCategory/AllCategoryProducts.js
@@ -0,0 +1,246 @@
+import React from 'react';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { RiDeleteBin5Line } from 'react-icons/ri';
+import Ratings from '../../features/Ratings';
+import pro6 from "../../../images/products/pro6.png"
+import pro7 from "../../../images/products/pro7.png"
+import pro8 from "../../../images/products/pro8.png"
+import pro10 from "../../../images/products/pro10.png"
+import pro11 from "../../../images/products/pro11.png"
+import pro12 from "../../../images/products/pro12.png"
+
+const AllCategoryProducts = () => {
+    return (
+        <div className='lg:grid lg:grid-cols-3 md:grid md:grid-cols-2 gap-5 my-5 font-normal'>
+            <div className='h-[350px] bg-white border border-[#F4F4F6] p-2 rounded-md mb-4'>
+                <div className='bg-[#F4F4F6] rounded-md'>
+                    <img className='w-[150px] h-[200px] mx-auto pt-4 relative' src={pro7} alt="" />
+                    <div className=' mb[-100px]'>
+                        <AiOutlineHeart className='absolute lg:ml-[220px] ml-[200px] mt-[-180px] text-gray-400 md:ml-[140px]' />
+                    </div>
+                </div>
+                <div className='mt-2'>
+                    <div>
+                        <Ratings />
+                        <p>Headrest Executive Mesh Office Chair set</p>
+                    </div>
+                    <div className='flex justify-between text-[#227ED2] font-medium mt-4'>
+                        <p>৳10500</p>
+                        <p><RiDeleteBin5Line /></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='h-[350px] bg-white border border-[#F4F4F6] p-2 rounded-md mb-4'>
+                <div className='bg-[#F4F4F6] rounded-md'>
+                    <img className='w-[150px] h-[200px] mx-auto pt-4 relative' src={pro8} alt="" />
+                    <div className=' mb[-100px]'>
+                        <AiOutlineHeart className='absolute lg:ml-[220px] ml-[200px] mt-[-180px] text-gray-400 md:ml-[140px]' />
+                    </div>
+                </div>
+                <div className='mt-2'>
+                    <div>
+                        <Ratings />
+                        <p>Headrest Executive Mesh Office Chair set</p>
+                    </div>
+                    <div className='flex justify-between text-[#227ED2] font-medium mt-4'>
+                        <p>৳10500</p>
+                        <p><RiDeleteBin5Line /></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='h-[350px] bg-white border border-[#F4F4F6] p-2 rounded-md mb-4'>
+                <div className='bg-[#F4F4F6] rounded-md'>
+                    <img className='w-[150px] h-[200px] mx-auto pt-4 relative' src={pro6} alt="" />
+                    <div className=' mb[-100px]'>
+                        <AiOutlineHeart className='absolute lg:ml-[220px] ml-[200px] mt-[-180px] text-gray-400 md:ml-[140px]' />
+                    </div>
+                </div>
+                <div className='mt-2'>
+                    <div>
+                        <Ratings />
+                        <p>Headrest Executive Mesh Office Chair set</p>
+                    </div>
+                    <div className='flex justify-between text-[#227ED2] font-medium mt-4'>
+                        <p>৳10500</p>
+                        <p><RiDeleteBin5Line /></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='h-[350px] bg-white border border-[#F4F4F6] p-2 rounded-md mb-4'>
+                <div className='bg-[#F4F4F6] rounded-md'>
+                    <img className='w-[150px] h-[200px] mx-auto pt-4 relative' src={pro10} alt="" />
+                    <div className=' mb[-100px]'>
+                        <AiOutlineHeart className='absolute lg:ml-[220px] ml-[200px] mt-[-180px] text-gray-400 md:ml-[140px]' />
+                    </div>
+                </div>
+                <div className='mt-2'>
+                    <div>
+                        <Ratings />
+                        <p>Headrest Executive Mesh Office Chair set</p>
+                    </div>
+                    <div className='flex justify-between text-[#227ED2] font-medium mt-4'>
+                        <p>৳10500</p>
+                        <p><RiDeleteBin5Line /></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='h-[350px] bg-white border border-[#F4F4F6] p-2 rounded-md mb-4'>
+                <div className='bg-[#F4F4F6] rounded-md'>
+                    <img className='w-[150px] h-[200px] mx-auto pt-4 relative' src={pro11} alt="" />
+                    <div className=' mb[-100px]'>
+                        <AiOutlineHeart className='absolute lg:ml-[220px] ml-[200px] mt-[-180px] text-gray-400 md:ml-[140px]' />
+                    </div>
+                </div>
+                <div className='mt-2'>
+                    <div>
+                        <Ratings />
+                        <p>Headrest Executive Mesh Office Chair set</p>
+                    </div>
+                    <div className='flex justify-between text-[#227ED2] font-medium mt-4'>
+                        <p>৳10500</p>
+                        <p><RiDeleteBin5Line /></p>
+                    </div>
+                </div>
+            </div>
+            <div className='h-[350px] bg-white border border-[#F4F4F6] p-2 rounded-md mb-4'>
+                <div className='bg-[#F4F4F6] rounded-md'>
+                    <img className='w-[150px] h-[200px] mx-auto pt-4 relative' src={pro12} alt="" />
+                    <div className=' mb[-100px]'>
+                        <AiOutlineHeart className='absolute lg:ml-[220px] ml-[200px] mt-[-180px] text-gray-400 md:ml-[140px]' />
+                    </div>
+                </div>
+                <div className='mt-2'>
+                    <div>
+                        <Ratings />
+                        <p>Headrest Executive Mesh Office Chair set</p>
+                    </div>
+                    <div className='flex justify-between text-[#227ED2] font-medium mt-4'>
+                        <p>৳10500</p>
+                        <p><RiDeleteBin5Line /></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='h-[350px] bg-white border border-[#F4F4F6] p-2 rounded-md mb-4'>
+                <div className='bg-[#F4F4F6] rounded-md'>
+                    <img className='w-[150px] h-[200px] mx-auto pt-4 relative' src={pro7} alt="" />
+                    <div className=' mb[-100px]'>
+                        <AiOutlineHeart className='absolute lg:ml-[220px] ml-[200px] mt-[-180px] text-gray-400 md:ml-[140px]' />
+                    </div>
+                </div>
+                <div className='mt-2'>
+                    <div>
+                        <Ratings />
+                        <p>Headrest Executive Mesh Office Chair set</p>
+                    </div>
+                    <div className='flex justify-between text-[#227ED2] font-medium mt-4'>
+                        <p>৳10500</p>
+                        <p><RiDeleteBin5Line /></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='h-[350px] bg-white border border-[#F4F4F6] p-2 rounded-md mb-4'>
+                <div className='bg-[#F4F4F6] rounded-md'>
+                    <img className='w-[150px] h-[200px] mx-auto pt-4 relative' src={pro8} alt="" />
+                    <div className=' mb[-100px]'>
+                        <AiOutlineHeart className='absolute lg:ml-[220px] ml-[200px] mt-[-180px] text-gray-400 md:ml-[140px]' />
+                    </div>
+                </div>
+                <div className='mt-2'>
+                    <div>
+                        <Ratings />
+                        <p>Headrest Executive Mesh Office Chair set</p>
+                    </div>
+                    <div className='flex justify-between text-[#227ED2] font-medium mt-4'>
+                        <p>৳10500</p>
+                        <p><RiDeleteBin5Line /></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='h-[350px] bg-white border border-[#F4F4F6] p-2 rounded-md mb-4'>
+                <div className='bg-[#F4F4F6] rounded-md'>
+                    <img className='w-[150px] h-[200px] mx-auto pt-4 relative' src={pro6} alt="" />
+                    <div className=' mb[-100px]'>
+                        <AiOutlineHeart className='absolute lg:ml-[220px] ml-[200px] mt-[-180px] text-gray-400 md:ml-[140px]' />
+                    </div>
+                </div>
+                <div className='mt-2'>
+                    <div>
+                        <Ratings />
+                        <p>Headrest Executive Mesh Office Chair set</p>
+                    </div>
+                    <div className='flex justify-between text-[#227ED2] font-medium mt-4'>
+                        <p>৳10500</p>
+                        <p><RiDeleteBin5Line /></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='h-[350px] bg-white border border-[#F4F4F6] p-2 rounded-md mb-4'>
+                <div className='bg-[#F4F4F6] rounded-md'>
+                    <img className='w-[150px] h-[200px] mx-auto pt-4 relative' src={pro10} alt="" />
+                    <div className=' mb[-100px]'>
+                        <AiOutlineHeart className='absolute lg:ml-[220px] ml-[200px] mt-[-180px] text-gray-400 md:ml-[140px]' />
+                    </div>
+                </div>
+                <div className='mt-2'>
+                    <div>
+                        <Ratings />
+                        <p>Headrest Executive Mesh Office Chair set</p>
+                    </div>
+                    <div className='flex justify-between text-[#227ED2] font-medium mt-4'>
+                        <p>৳10500</p>
+                        <p><RiDeleteBin5Line /></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='h-[350px] bg-white border border-[#F4F4F6] p-2 rounded-md mb-4'>
+                <div className='bg-[#F4F4F6] rounded-md'>
+                    <img className='w-[150px] h-[200px] mx-auto pt-4 relative' src={pro11} alt="" />
+                    <div className=' mb[-100px]'>
+                        <AiOutlineHeart className='absolute lg:ml-[220px] ml-[200px] mt-[-180px] text-gray-400 md:ml-[140px]' />
+                    </div>
+                </div>
+                <div className='mt-2'>
+                    <div>
+                        <Ratings />
+                        <p>Headrest Executive Mesh Office Chair set</p>
+                    </div>
+                    <div className='flex justify-between text-[#227ED2] font-medium mt-4'>
+                        <p>৳10500</p>
+                        <p><RiDeleteBin5Line /></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='h-[350px] bg-white border border-[#F4F4F6] p-2 rounded-md mb-4'>
+                <div className='bg-[#F4F4F6] rounded-md'>
+                    <img className='w-[150px] h-[200px] mx-auto pt-4 relative' src={pro12} alt="" />
+                    <div className=' mb[-100px]'>
+                        <AiOutlineHeart className='absolute lg:ml-[220px] ml-[200px] mt-[-180px] text-gray-400 md:ml-[140px]' />
+                    </div>
+                </div>
+                <div className='mt-2'>
+                    <div>
+                        <Ratings />
+                        <p>Headrest Executive Mesh Office Chair set</p>
+                    </div>
+                    <div className='flex justify-between text-[#227ED2] font-medium mt-4'>
+                        <p>৳10500</p>
+                        <p><RiDeleteBin5Line /></p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default AllCategoryProducts;
--- src/components/pages/productCategory/ProductCategory.js
+++ src/components/pages/productCategory/ProductCategory.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Container from '../Container';
 import FilterProduct from './FilterProduct';
+import AllCategoryProducts from './AllCategoryProducts';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
 const ProductCategory = () => {
     return (
@@ -11,19 +13,43 @@ const ProductCategory = () => {
                         <FilterProduct />
                     </div>
 
-                    <div className=' w-full md:w-[82%] mx-auto'>
-                        <div className='flex items-center justify-between mt-10'>
-                            <div className='flex items-center gap-1'>
-                                <p className='text-[#757575]'>Showing  <span className='text-gray-700'>20</span> of</p>
-                                <p className='text-[#757575]'><span className='text-gray-700'>160</span> product</p>
-                            </div>
-                            <div className='flex items-center'>
-                                <small className='text-sm text-[#757575] mr-2'>sort by: </small>
-                                <div>
-                                    <select className='bg-gray-100 border border-gray-200 text-sm'>
-                                        <option>Newest Items</option>
-                                    </select>
+                    <div className=' w-full md:w-[82%] mx-auto px-8'>
+                        <div className=''>
+                            <div className='flex items-center justify-between mt-10'>
+                                <div className='flex items-center gap-1'>
+                                    <p className='text-[#757575]'>Showing  <span className='text-gray-700'>20</span> of</p>
+                                    <p className='text-[#757575]'><span className='text-gray-700'>160</span> product</p>
                                 </div>
+                                <div className='flex items-center'>
+                                    <small className='text-sm text-[#757575] mr-2'>sort by: </small>
+                                    <div>
+                                        <select className='bg-gray-100 border border-gray-200 text-sm'>
+                                            <option>Newest Items</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* ALL PCATEGORY PRODUCTS START */}
+                            <AllCategoryProducts />
+                            {/* ALL PCATEGORY PRODUCTS END */}
+                        </div>
+                        <div className='flex items-center justify-end text-sm rounded-sm mb-5'>
+                            <div>
+                                <button className='flex items-center gap-2 px-2 py-1  bg-white border border-[#E9E9E9] text-[#0198E9]'>
+                                    <IoIosArrowBack />
+                                    <p>Previous</p>
+                                </button>
+                            </div>
+                            <div className='flex items-center text-[#0198E9] font-semibold'>
+                                <small className='border border-[#E9E9E9] px-3 py-1 bg-[#0198E9] text-white'>1</small>
+                                <small className='border border-[#E9E9E9] px-3 py-1 bg-white'>2</small>
+                                <small className='border border-[#E9E9E9] px-3 py-1 bg-white'>3</small>
+                            </div>
+                            <div>
+                                <button className='flex items-center gap-2 px-3 py-1  bg-white border border-[#E9E9E9] text-[#0198E9]'>
+                                    <p>Next</p>
+                                    <IoIosArrowForward />
+                                </button>
                             </div>
                         </div>
                     </div>
