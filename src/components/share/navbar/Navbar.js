21a1e8957c6d05d4a445769359b4b9446058f75e
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-01-29 23:53:50 +0600

    logo added side navbar added

--- src/components/share/navbar/Navbar.js
+++ src/components/share/navbar/Navbar.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 import { BiSearch } from 'react-icons/bi';
 import { TiUserOutline } from 'react-icons/ti';
 import { BsBasket } from 'react-icons/bs';
+import logo from "../../../images/footerimg/logo.png";
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -15,7 +16,7 @@ const Navbar = () => {
     return (
         <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 '>
             <div>
-                <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+                <img src={logo} alt="" />
             </div>
             <div className='hidden lg:block'>
                 <div className='flex items-center '>
@@ -28,17 +29,14 @@ const Navbar = () => {
                 <NavLink className="flex items-center gap-1"><AiOutlineHeart /> <small>Wishlist</small></NavLink>
                 <NavLink className="flex items-center gap-1"><BsBasket /><small>My Cart</small> <div className='bg-[#FF5D5D] w-[14px] h-[14px] rounded-full relative'><small className='absolute mt-[-1px] ml-[5px] text-xs text-white'>1</small></div></NavLink>
             </div>
-            
+
             <div onClick={handleNav} className='block lg:hidden'>
                 {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
             </div>
             <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-                <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
-                <li className='p-4 border-b border-gray-600 text-white'>Home</li>
-                <li className='p-4 border-b border-gray-600'>Company</li>
-                <li className='p-4 border-b border-gray-600'>Resources</li>
-                <li className='p-4 border-b border-gray-600'>About</li>
-                <li className='p-4'>Contact</li>
+                <NavLink className="flex items-center gap-1 text-white p-4 border-b border-gray-600"><TiUserOutline /><small>Login</small></NavLink>
+                <NavLink className="flex items-center gap-1 text-white p-4 border-b border-gray-600"><AiOutlineHeart /> <small>Wishlist</small></NavLink>
+                <NavLink className="flex items-center gap-1 text-white p-4 border-b border-gray-600"><BsBasket /><small>My Cart</small> <div className='bg-[#FF5D5D] w-[14px] h-[14px] rounded-full relative'><small className='absolute mt-[-1px] ml-[5px] text-xs text-white'>1</small></div></NavLink>
             </ul>
         </div>
     );
